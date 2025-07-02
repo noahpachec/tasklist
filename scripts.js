ae054400733f01f81bb6d4ca97c92cd463b60174
noahpachec <noahpachec@gmail.com>
2025-07-02 21:09:28 +1000

    Added drag listeners '#' will be ignored, and an empty message aborts the commit.

--- scripts.js
+++ scripts.js
@@ -1,24 +1,33 @@
 const body = document.getElementsByName("body")
-body.addEventListener("keydown", function() {
-    if (key === "Enter") {
+document.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
         createTask()
     }
 })
 
+let draggedItem;
+function addDragHandler(li) {
+            li.addEventListener("dragstart", (event) => {
+                console.log("dragging");
+                draggedItem = li
+            })
+        }
 
 function createTask() {
     const input = document.getElementById("input").value
     const text = input.trim()
     if (text !== "") {
         const li = document.createElement("li")
+        li.setAttribute("draggable", true)
         li.textContent = text
         document.getElementById("tasks").appendChild(li)
         li.classList.add("task-items")
         li.classList.add("incomplete")
         removeButton(li)
         completedButton(li)
+        addDragHandler(li)
+        document.getElementById("input").value = ""
     }
-    console.log("hello")  
 }
 
 function removeButton(li) {
